927d517a80f930e491ae2c8e275f6861a6b05c40
gabrielagithub <31082412+gabrielagithub@users.noreply.github.com>
2024-03-18 22:38:57 -0300

    separação de arquivos

---
+++ script.js
@@ -0,0 +1,76 @@
+const URL = "https://gabrielagithub.github.io/drum/";
+let model, webcam, ctx, labelContainer, maxPredictions;
+
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    model = await tmPose.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    const size = 200;
+    const flip = true;
+    webcam = new tmPose.Webcam(size, size, flip);
+    await webcam.setup();
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    const canvas = document.createElement("canvas");
+    canvas.width = size;
+    canvas.height = size;
+    document.body.appendChild(canvas);
+    ctx = canvas.getContext("2d");
+
+    // Selecione o elemento label-container
+    labelContainer = document.getElementById("label-container");
+    // Verifique se o elemento label-container existe antes de tentar adicionar elementos filho
+    if (labelContainer) {
+        for (let i = 0; i < maxPredictions; i++) {
+            labelContainer.appendChild(document.createElement("div"));
+        }
+    } else {
+        console.error("Elemento label-container não encontrado.");
+    }
+}
+
+async function loop(timestamp) {
+    webcam.update();
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+async function predict() {
+    const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+    const prediction = await model.predict(posenetOutput);
+
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction = prediction[i].className;
+        const probability = prediction[i].probability;
+
+        // Atualize a interface do usuário com a previsão atual
+        if (labelContainer && labelContainer.childNodes[i]) {
+            labelContainer.childNodes[i].innerHTML = `${classPrediction}: ${probability.toFixed(2)}`;
+        }
+
+        // Verifique se a probabilidade da previsão atual é maior que o limiar
+        if (probability > 0.5) {
+            // Execute a função de reprodução de som correspondente à classe de pose
+            playSound(classPrediction);
+        }
+    }
+
+    // Desenhe as poses na tela
+    drawPose(pose);
+}
+
+function drawPose(pose) {
+    if (webcam.canvas) {
+        ctx.drawImage(webcam.canvas, 0, 0);
+        if (pose) {
+            const minPartConfidence = 0.5;
+            tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+            tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+        }
+    }
+}
+
